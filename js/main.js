20a9dd60cc6c458e691fa95ac7b51b34cf48f92b
Marcos Vidal <markos_ari@hotmail.com>
2023-12-21 20:01:43 -0300

    ·ADD animations -> fadeIn, fadeOut & typing

--- js/main.js
+++ js/main.js
@@ -2,15 +2,24 @@
 function displayToast() {
     const toast = document.getElementById("toast")
     const btnToast = document.getElementById("btn-toast")
-    if (toast.style.display === "none") {
-        toast.style.display = "block"
+
+    if (toast.className.includes("fade-out")) {
+        toast.className = toast.className.replace("fade-out", "fade-in")
         btnToast.innerHTML = "Hide Toast"
+        
         setTimeout(() => {
-            toast.style.display = "none"
+            toast.className = toast.className.replace("fade-in", "fade-out")
             btnToast.innerHTML = "Show Toast"
         }, 7000);
-    } else{
-        toast.style.display = "none"
+    } else if (toast.className.includes("fade-in")) {
+        toast.className = toast.className.replace("fade-in", "fade-out")
         btnToast.innerHTML = "Show Toast"
+    } else {
+        toast.className = toast.className.concat(" fade-in")
+        btnToast.innerHTML = "Hide Toast"
+        setTimeout(() => {
+            toast.className = toast.className.replace("fade-in", "fade-out")
+            btnToast.innerHTML = "Show Toast"
+        }, 7000);
     }
 }
